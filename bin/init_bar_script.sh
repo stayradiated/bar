67a847cc8b3f539fd87747a77744df7c1f155b37
George Czabania <george@czabania.com>
2013-12-17 20:37:43 +1300

    Use date instead of clock.c

--- bin/init_bar_script.sh
+++ bin/init_bar_script.sh
@@ -5,8 +5,12 @@ PANEL_FIFO=/tmp/bar.fifo
 [ -e $PANEL_FIFO ] && rm $PANEL_FIFO
 mkfifo $PANEL_FIFO
 
-# Run bar
-cat $PANEL_FIFO | node ~/Projects/Bar/bin/printer.js &
+# Run bar and keep reading from fifo
+tail -n +1 -f $PANEL_FIFO | node ~/Projects/Bar/bin/printer.js & 
 
-# Display time
-# ~/Github/baskerville/sutils/clock -s -i 10 -f 'S%d %b - %I:%M %p' > $PANEL_FIFO &
+while true
+do
+  date "+S%d %b - %I:%M %p" > $PANEL_FIFO
+  sleep 1
+done
+
