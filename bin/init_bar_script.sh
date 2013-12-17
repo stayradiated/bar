53460dd788d71997992091776f52f61efcd193b6
George Czabania <george@czabania.com>
2013-12-17 20:56:13 +1300

    Extend time

--- bin/init_bar_script.sh
+++ bin/init_bar_script.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 PANEL_FIFO=/tmp/bar.fifo
+PRINTER=~/Projects/Bar/bin/printer.js
 
 [ -e $PANEL_FIFO ] && rm $PANEL_FIFO
 mkfifo $PANEL_FIFO
 
 # Run bar and keep reading from fifo
-tail -n +1 -f $PANEL_FIFO | node ~/Projects/Bar/bin/printer.js & 
+tail -n +1 -f $PANEL_FIFO | node $PRINTER & 
 
 while true
 do
   date "+S%d %b - %I:%M %p" > $PANEL_FIFO
-  sleep 1
+  sleep 5
 done
 
